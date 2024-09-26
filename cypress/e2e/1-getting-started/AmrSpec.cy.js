701490977835a9b46c3ce7c8a04a0f229d936393
t <t@192.168.1.3>
2024-09-26 19:07:05 +0300

    Test from amr Mirige and push

---
+++ cypress/e2e/1-getting-started/AmrSpec.cy.js
@@ -0,0 +1,10 @@
+describe('Testsync01143236791 ', () => {
+  it('Sync Amr 01143236791', () => {
+    // cy.contains('Connect').should('contain', 'Connect')
+    // cy.contains('Connect').click()
+    // cy.get('.SyncInput').type('http://192.168.100.64:8000')
+    cy.visit('https://www.currency-price.com/')
+
+})
+})
+
